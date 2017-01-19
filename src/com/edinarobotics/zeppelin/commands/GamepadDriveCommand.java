5589bc923c0ee1e064c0d918f3ff54e925380234
ElliottOlson <elliotto17258@apps.edina.k12.mn.us>
2017-01-18 19:22:40 -0600

    Implemented GamepadDriveCommand

---
+++ src/com/edinarobotics/zeppelin/commands/GamepadDriveCommand.java
@@ -0,0 +1,43 @@
+package com.edinarobotics.zeppelin.commands;
+
+import com.edinarobotics.utils.gamepad.Gamepad;
+import com.edinarobotics.zeppelin.Components;
+import com.edinarobotics.zeppelin.subsystems.Drivetrain;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class GamepadDriveCommand extends Command {
+
+	private Drivetrain drivetrain;
+	private Gamepad gamepad0;
+	
+    public GamepadDriveCommand(Gamepad gamepad0) {
+    	super("gamepaddrivecommand");
+    	this.gamepad0 = gamepad0;
+    	drivetrain = Components.getInstance().drivetrain;
+    	requires(drivetrain);
+    }
+
+    protected void initialize() {
+    }
+
+    protected void execute() {
+    	double verticalStrafe = gamepad0.getLeftY();
+    	double horizontalStrafe = gamepad0.getLeftX();
+    	double rotation = gamepad0.getRightX();
+    	
+    	drivetrain.setDrivetrain(verticalStrafe, horizontalStrafe, rotation);
+    }
+
+    protected boolean isFinished() {
+        return false;
+    }
+
+    protected void end() {
+    	drivetrain.setDrivetrain(0, 0, 0);
+    }
+
+    protected void interrupted() {
+    	end();
+    }
+}
