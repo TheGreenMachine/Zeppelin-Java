6ef542e2883cb7ca95a5c5f9fd885c58317cc4cc
ElliottOlson <elliotto17258@apps.edina.k12.mn.us>
2017-01-18 19:02:47 -0600

    Created Drivetrain object inside of Components class

--- src/com/edinarobotics/zeppelin/Components.java
+++ src/com/edinarobotics/zeppelin/Components.java
@@ -1,12 +1,22 @@
 package com.edinarobotics.zeppelin;
 
+import com.edinarobotics.zeppelin.subsystems.Drivetrain;
+
 public class Components {
    
 	private static Components instance;
 	
+	public Drivetrain drivetrain;
+	
+	private static final int FRONT_LEFT = 1;
+	private static final int FRONT_RIGHT = 2;
+	private static final int REAR_LEFT = 3;
+	private static final int REAR_RIGHT = 4;
+	private static final int MIDDLE = 5;
+	
+	
 	private Components() {
-		
-		
+		drivetrain = new Drivetrain(FRONT_RIGHT, FRONT_LEFT, MIDDLE, REAR_RIGHT, REAR_LEFT);
 		
 	}
 	
