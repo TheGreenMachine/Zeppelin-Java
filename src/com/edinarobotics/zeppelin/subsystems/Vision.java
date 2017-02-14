ebfccde55961233cec95033bb7a23fc3e75b1fcc
ElliottOlson <elliotto17258@apps.edina.k12.mn.us>
2017-02-13 20:29:38 -0600

    Added ability to calculate strafing speed in Vision subsystem

--- src/com/edinarobotics/zeppelin/subsystems/Vision.java
+++ src/com/edinarobotics/zeppelin/subsystems/Vision.java
@@ -1,12 +1,25 @@
 package com.edinarobotics.zeppelin.subsystems;
 
 import com.edinarobotics.utils.subsystems.Subsystem1816;
+import com.edinarobotics.zeppelin.Components;
 
 import edu.wpi.first.wpilibj.SerialPort;
 
 public class Vision extends Subsystem1816 {
 
 	private SerialPort serial;
+
+	private Drivetrain drivetrain;
+	private double verticalStrafe;
+	private double horizontalStrafe;
+
+	private final int CAMERA_WIDTH = 640;
+	private final int CAMERA_HEIGHT = 480;
+
+	private final int X_TOLERANCE = 30;
+	private final int Y_TOLERANCE = 30;
+	private final int SLOW_RANGE = 30;
+	private final int ENDING_TOLERANCE = 3;
 
 	private String kInput;
 	private double kX;
@@ -20,7 +33,34 @@ public class Vision extends Subsystem1816 {
 
 	@Override
 	public void update() {
+		drivetrain.setDrivetrain(verticalStrafe, horizontalStrafe, 0.0);
+	}
+	
+	public void initialize() {
+		drivetrain = Components.getInstance().drivetrain;
+	}
 
+	public void calculateStrafe() {
+		double deltaVision = CAMERA_WIDTH / 2 - getkX();
+															
+		double strafe;
+
+		if (deltaVision < SLOW_RANGE && deltaVision > -SLOW_RANGE) {
+			if (deltaVision > ENDING_TOLERANCE)
+				strafe = 0.5 / 1.5;
+			else if (deltaVision < -3)
+				strafe = -0.5 / 1.5;
+			else
+				strafe = 0;
+		} else {
+			if (deltaVision > 0)
+				strafe = 0.5;
+			else
+				strafe = -0.5;
+		}
+		
+		horizontalStrafe = strafe;
+		update();
 	}
 
 	public void readInput() {
@@ -31,8 +71,7 @@ public class Vision extends Subsystem1816 {
 		String input = serial.readString();
 
 		if (input.length() > 0) {
-			if (input.substring(0, 1).equals("{") && input.substring(input.length() - 1, 
-					input.length()).equals("}")) {
+			if (input.substring(0, 1).equals("{") && input.substring(input.length() - 1, input.length()).equals("}")) {
 				space1 = input.indexOf(' ');
 				space2 = input.substring(space1 + 1).indexOf(' ') + space1 + 1;
 				endBracket = input.indexOf('}');
@@ -61,4 +100,17 @@ public class Vision extends Subsystem1816 {
 			}
 		}
 	}
+
+	public double getkX() {
+		return kX;
+	}
+
+	public double getkY() {
+		return kY;
+	}
+
+	public double getArea() {
+		return area;
+	}
+
 }
