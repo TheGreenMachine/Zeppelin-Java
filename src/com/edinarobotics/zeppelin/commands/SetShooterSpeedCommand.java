4833a765bff992103324ae6a8684d649f92a6949
ElliottOlson <elliotto17258@apps.edina.k12.mn.us>
2017-02-13 18:41:11 -0600

    Created SetShooterSpeedCommand for controlling whether the shooter should be active or not.

---
+++ src/com/edinarobotics/zeppelin/commands/SetShooterSpeedCommand.java
@@ -0,0 +1,32 @@
+package com.edinarobotics.zeppelin.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class SetShooterSpeedCommand extends Command {
+
+	@Override
+	protected void initialize() {
+		
+	}
+
+	@Override
+	protected void execute() {
+		
+	}
+
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
+
+	@Override
+	protected void end() {
+		
+	}
+
+	@Override
+	protected void interrupted() {
+		
+	}
+
+}
