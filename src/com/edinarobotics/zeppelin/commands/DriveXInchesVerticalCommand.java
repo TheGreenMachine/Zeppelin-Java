0a39e354317691473269e053eb4ca3d4279683be
ElliottOlson <elliotto17258@apps.edina.k12.mn.us>
2017-02-17 17:08:38 -0600

    Updated DriveXInchesCommand with more correct conversion numbers

--- src/com/edinarobotics/zeppelin/commands/DriveXInchesVerticalCommand.java
+++ src/com/edinarobotics/zeppelin/commands/DriveXInchesVerticalCommand.java
@@ -11,106 +11,85 @@ public class DriveXInchesVerticalCommand extends Command {
 	private Drivetrain drivetrain;
 	private int inches;
 	private int ticks;
-	private final int CONVERSION_FACTOR = 39;
+	private final int CONVERSION_FACTOR = 34;
 	private final int TARGET_THRESHOLD = 5;
 
 	private AHRS gyro;
 	private double heading;
 
-//	private PrintWriter printWriter;
-	
 	public DriveXInchesVerticalCommand(int inches) {
 		super("drivexinchesverticalcommand");
 		drivetrain = Components.getInstance().drivetrain;
 		gyro = Components.getInstance().navX;
 		this.inches = inches;
+		ticks = (inches * CONVERSION_FACTOR) + drivetrain.getFrontLeft().getEncPosition();
 		requires(drivetrain);
 	}
 
 	@Override
 	protected void initialize() {
 		heading = gyro.getAngle();
-		drivetrain.getFrontLeft().setPosition(0);
-		ticks = (inches * CONVERSION_FACTOR) + drivetrain.getFrontLeft().getEncPosition();
-		System.out.println(drivetrain.getFrontLeft().getEncPosition());
-		System.out.println(ticks);
-//		System.out.println("Initializing...");
-		
-//		try {
-//			printWriter = new PrintWriter(new File("/usr/lvuser/logger.csv"));
-//		}
-//		catch(Exception e) {
-//			System.out.println(e.getMessage());
-//		}
+		System.out.println("Initializing...");
 	}
 
 	@Override
 	protected void execute() {
-		int currentLeftValue = drivetrain.getFrontLeft().getEncPosition();
+		double currentLeftValue = drivetrain.getFrontLeft().getEncPosition();
 		double currentHeading = gyro.getAngle();
 
 		if (ticks > currentLeftValue) {
 			if (currentLeftValue > (ticks - 1000)) {
 
 				if (Math.abs(currentHeading - heading) > 0.3) {
-					drivetrain.setDrivetrain(0.30, 0.0, -(currentHeading - heading) * .1);
+					drivetrain.setDrivetrain(0.35, 0.0, -(currentHeading - heading) * .05);
+					System.out.println("1.2");
 				} else {
-					drivetrain.setDrivetrain(0.30, 0, 0);
+					drivetrain.setDrivetrain(0.35, 0, 0);
+					System.out.println("1.1");
 				}
-//				System.out.println("1");
+				System.out.println("1");
 			} else {
 				if (Math.abs(currentHeading - heading) > 0.3) {
-					drivetrain.setDrivetrain(0.70, 0, -(currentHeading - heading) * .1);
+					drivetrain.setDrivetrain(0.70, 0, -(currentHeading - heading) * .05);
 				} else {
 					drivetrain.setDrivetrain(0.70, 0, 0);
 				}
-//				System.out.println("2");
+				System.out.println("2");
 			}
 		} else {
 			if (currentLeftValue < (ticks + 1000)) {
 				if (Math.abs(currentHeading - heading) > 0.3) {
-					drivetrain.setDrivetrain(-0.30, 0, (currentHeading - heading) * .1);
+					drivetrain.setDrivetrain(-0.35, 0, -(currentHeading - heading) * .05);
 				} else {
-					drivetrain.setDrivetrain(-0.30, 0, 0);
+					drivetrain.setDrivetrain(-0.35, 0, 0);
 				}
-//				System.out.println("3");
+				System.out.println("3");
 			} else {
 				if (Math.abs(currentHeading - heading) > 0.3) {
-					drivetrain.setDrivetrain(-0.70, 0, (currentHeading - heading) * .1);
+					drivetrain.setDrivetrain(-0.70, 0, -(currentHeading - heading) * .05);
 				} else {
 					drivetrain.setDrivetrain(-0.70, 0, 0);
 				}
-//				System.out.println("4");
+				System.out.println("4");
 			}
 		}
 
 		System.out.println("Left value: " + currentLeftValue + "; Left target: " + ticks);
-//		System.out.println("Current heading: " + currentHeading + "; Target heading: " + heading);
-		
-//		System.out.println("CV: " + currentLeftValue + "; T: " + ticks + "; V: " + drivetrain.getFrontLeft().get());
-		
-//		StringBuilder sb = new StringBuilder();
-//		
-//		sb.append(currentLeftValue);
-//		sb.append(',');
-//		sb.append(ticks);
-//		sb.append(',');
-//		sb.append(drivetrain.getFrontLeft().get());
-//		sb.append('\n');
-//
-//		printWriter.write(sb.toString());
+		System.out.println("Current heading: " + currentHeading + "; Target heading: " + heading);
 	}
 
 	@Override
 	protected boolean isFinished() {
+		if (Math.abs(drivetrain.getFrontLeft().getEncPosition() - ticks) < TARGET_THRESHOLD)
+			System.out.println("Finished");
+			
 		return Math.abs(drivetrain.getFrontLeft().getEncPosition() - ticks) < TARGET_THRESHOLD;
+	
 	}
 
 	@Override
 	protected void end() {
 		drivetrain.setDrivetrain(0.0, 0.0, 0.0);
-		drivetrain.getFrontLeft().setPosition(0);
-//		printWriter.close();
 	}
 
 	@Override
